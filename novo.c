c5a70c7e38893f017f22b0bbf5529ccf83c0d0e8
Salatiel2909 <202721340+Salatiel2909@users.noreply.github.com>
2025-03-12 13:43:02 -0300

    Update novo.c

--- novo.c
+++ novo.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main(){
+  char nome[50];
+  int idade;
+
+  printf("seu nome: \n");
+  scanf("%s", &nome);
+
+  printf("digite sua idade: \n");
+  scanf("%d", &idade);
+  
+
+  printf("suas medias escolares\n");
+  printf("digite a materia: \n");
+  
+
+
+
+  scanf("
+
+    
+  
+
+
+
+
+
+
+
+
+
+  
+}
+  
